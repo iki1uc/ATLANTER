247eea12cb6b3a13d306a372cc054bbecefd03b8
iki1uc <entertrainer@web.de>
2026-08-29 17:26:14 +0200

    Update toolomat_flaeche_48.js

--- our/toolomat_flaeche_48.js
+++ our/toolomat_flaeche_48.js
@@ -11,3 +11,9 @@ function AIR_RESPO_NEW() {
         ROOT: true
     };
 }
+export function flaeche48(values, side) {
+  const len = values.length;
+  const pos = (len + (side === 'A' ? 3 : 7)) % 16;
+  const mass = len * (side === 'A' ? 2 : 4);
+  return { POS: pos, MASS: mass, SIDE: side };
+}
